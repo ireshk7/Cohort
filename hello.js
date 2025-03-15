010784b76d7b8c8baaaf1c5b73e80598b338cd2e
Iresh Kotabagi <iresh2003@gmail.com>
2025-03-16 00:10:11 +0530

    Javascript-1

---
+++ hello.js
@@ -0,0 +1,16 @@
+function printChai(){
+    console.log("Hello Bitch ");   
+}
+
+function brush(numberOfBrush){
+
+    console.log(`Haanji le ${numberOfBrush} aaya brush` )
+} 
+
+function addTwo(num1,num2){
+    return num1+num2
+}
+
+printChai();
+brush(5);
+console.log(addTwo(1,1));
